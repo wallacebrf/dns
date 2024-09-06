4a629bcbcb91a9a5bec97c536fcfe7f78e9e5dcf
wallacebrf <62187108+wallacebrf@users.noreply.github.com>
2024-09-06 12:46:10 -0500

    Update DNS_block_lists_all.php
    updating to remove duplicate entires

--- DNS_block_lists_all.php
+++ DNS_block_lists_all.php
@@ -38,97 +38,6 @@ $from_email_address="from@email.com";
 //SCRIPT START
 //*****************************************************************
 
-//import needed libraries for email sending 
-require '/volume1/web/admin/vendor/phpmailer/phpmailer/src/Exception.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-
-
-//get listing of all existing text files from the previous execution of the script. this will allow the script to send a notification email if a new file is created that did not previoously exist
-//this is important since a new file would have to be added to a fortigate's configuration under the "external connectors" 
-$currentFile="";
-$theFiles=[];
-//opens the directory for reading
-$dp = opendir(getcwd())
-	or die("<br /><font color=\"#FF0000\">Cannot Open The Directory </font><br>");
-	
-//add all files in directory to $theFiles array
-while ($currentFile !== false){
-	$currentFile = readDir($dp);
-	$theFiles[] = $currentFile;
-} // end while
-	
-//because we opened the dir, we need to close it
-closedir($dp);
-	
-//sorts all the files
-rsort ($theFiles);
-	
-foreach ($theFiles as $currentFile){
-	if ($currentFile != "." && $currentFile != ".." && $currentFile != "list_files.php" && $currentFile != "DNS_block_lists_all.php" && $currentFile != ""){
-		if ($refresh_data){
-			//if we are refreshing data, delete all files including the files previously downloaded 
-			unlink("/volume1/web/DNS_FG-91G/".$currentFile."");
-		}else{
-			//if we are NOT refreshing data, then we do NOT want to delete any of the "master" files downloaded from the internet
-			if(!str_contains($currentFile, 'master')){
-				unlink("/volume1/web/DNS_FG-91G/".$currentFile."");
-			}
-		}
-	}
-}
-
-
-//function to send email notifications if new files are created so the fortigate external connections list can be updated
-function send_email($file_name, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address){
-	date_default_timezone_set('Etc/UTC');
-
-
-	require "/volume1/web/admin/vendor/autoload.php";
-
-	//Create an instance; passing `true` enables exceptions
-	$mail = new PHPMailer(true);
-
-	try {
-		//Server settings
-		//$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-		$mail->isSMTP();                                            //Send using SMTP
-		$mail->Host       = ''.$smtp_server.'';                     //Set the SMTP server to send through
-		if($SMTPAuth_type==1){
-			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-		}else{
-			$mail->SMTPAuth   = false;                                   //Disable SMTP authentication
-		}
-		$mail->Username   = ''.$smtp_user.'';                     //SMTP username
-		$mail->Password   = ''.$smtp_pass.'';                               //SMTP password
-		if($SMTPSecure_type=="ENCRYPTION_STARTTLS"){
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-		}else if($SMTPSecure_type=="ENCRYPTION_SMTPS"){
-			$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable SSL
-		}
-		$mail->Port       = $smtp_port;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-		//Recipients
-		$mail->setFrom(''.$from_email_address.'');
-		
-		$mail->addAddress(''.$email_address.'');     //Add a recipient
-		
-		$mail->addReplyTo(''.$from_email_address.'');
-			
-		//Content
-		$mail->isHTML(true);                                  //Set email format to HTML
-		$mail->Subject = "New File - DNS Block Lists";
-		$mail->Body    = "New DNS Block List File \"".$file_name."\" has been added";
-		$mail->AltBody = "New DNS Block List File \"".$file_name."\" has been added";
-
-		$mail->send();
-		//echo 'Message has been sent';
-	} catch (Exception $e) {
-		echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-	}
-}
-
-
 //function to determine if a website is available or not
 //got code from https://stackoverflow.com/questions/24270027/how-to-check-if-website-is-accessible
 function urlExists($url=NULL)  
@@ -147,8 +56,6 @@ function urlExists($url=NULL)
         return false;  
     }  
 }  
-
-$domains_blocked_counter=0;
 
 //download the data from the desired URL block list
 function download_data($outputfile_local, $URL_local){
@@ -179,85 +86,7 @@ function download_data($outputfile_local, $URL_local){
 		}else{
 			print "<p style=\"color:red;\">".$URL_local." contents could not be downloaded</p>";
 		}
-	//}else{
-	//	print "<p style=\"color:red;\">".$URL_local." did not return as available</p>";
-	//}
 }
-
-//this function is needed because the fortigate external thread connectors can handle only 131,072 entries per list
-//however many lists have well over this limit. also, some sources that may be desired are much less than 131,000 entries, 
-//this allows aggregating multiple small lists into one file for the fortigate to process to maximize effectiveness as fortigates are also limited to the number of external threat feeds allowed
-function process_data($outputfile_local, $file_name, $debug_local, $array_to_search, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address){
-
-	$domains_blocked_counter_local=0; //how many domains have been added per the current file?
-	$data = file_get_contents($outputfile_local);
-	
-	$lines = explode("\n",$data); #make an array with all of the data in the file so we can process the data line by line
-
-	$counter = 1;
-
-	$handle = fopen( __DIR__ .'/'.$file_name.'.'.$counter.'.txt','wb');
-	if (!in_array(''.$file_name.'.'.$counter.'.txt', $array_to_search)) {
-		print "The file \"".$file_name.".".$counter.".txt\" is NEW!<br>";
-		send_email("".$file_name.".".$counter.".txt", $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
-	}
-	if ($debug_local){
-		$handle2 = fopen( __DIR__ .'/'.$file_name.'.debug.'.$counter.'.txt','wb');
-		if (!in_array(''.$file_name.'.debug.'.$counter.'.txt', $array_to_search)) {
-			print "The file \"".$file_name.".debug.".$counter.".txt\" is NEW!<br>";
-			send_email("".$file_name.".debug.".$counter.".txt", $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
-		}
-	}
-
-	$lineswritten = 0;
-	foreach ($lines as $line) {
-		
-		//many of these lists have comments starting with the "#" character, we will remove these lines if they START with a "#"
-		$character = "#";
-	   $line = preg_replace('/' . preg_quote($character, '/') . '.*/', '', $line);
-	   
-	   //some lines already have a return character at the end of the line, we do not want this so let's remove it. 
-	   $character = "\r";
-	   $line = preg_replace('/' . preg_quote($character, '/') . '.*/', '', $line);
-	  
-	  
-	  //other lines have comments or other data towards the end of the line after the domain name entry
-	  //we need to remove that information, and we need to perform additional filtering to remove empty lines
-	  if ($line!="" && $line!="\n" && $line!="\r" && $line!="\n\r" && $line!="\r\n" && strlen($line) > 6 && mb_strlen($line) > 6){
-		  if ($lineswritten!=0) fwrite($handle, chr(0x0D).chr(0x0A)); 
-		  fwrite($handle,$line);
-		  $domains_blocked_counter_local=$domains_blocked_counter_local+1;
-		  if ($debug_local){
-			fwrite($handle2,"".$lineswritten.": \"".$line."\"\n");
-		  }
-		  $lineswritten = $lineswritten+1;
-	  }
-	  // did we write 131,000 lines? Close file, increase counter and open next output file. Note fortigate external connectors have a max line limit of 131,072, we will save slightly less than this
-	  if ($lineswritten == 131000) {
-		  if ($debug_local){
-			echo "<br>".$file_name.".".$counter.".txt created";
-		  }
-		fclose($handle);
-		$counter = $counter+1;
-		$handle = fopen( __DIR__ .'/'.$file_name.'.'.$counter.'.txt','wb');
-		if (!in_array(''.$file_name.'.'.$counter.'.txt', $array_to_search)) {
-			print "The file \"".$file_name.".".$counter.".txt\" is NEW!<br>";
-			send_email("".$file_name.".".$counter.".txt", $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
-		}
-		$lineswritten = 0;
-	  }
-	}
-	fclose($handle);
-	if ($debug_local){
-		fclose($handle2);
-	}
-	if ($debug_local){
-		echo "<br>".$file_name.".".$counter.".txt created<br><b>Number of domains blocked in the various \"".$file_name."\" files: ".$domains_blocked_counter_local."</b>";
-		echo "<br><br>";
-	}
-	return $domains_blocked_counter_local;
-}
-
 
 $outputfile = 'master1.txt';
 if ($refresh_data){
@@ -304,8 +133,6 @@ if ($refresh_data){
 	$URL="https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt";
 	download_data($outputfile, $URL);
 }
-
-$domains_blocked_counter=$domains_blocked_counter + process_data($outputfile, "web_block1", $debug, $theFiles, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
 
 //*******************************************************************************************************************
 //*******************************************************************************************************************
@@ -358,8 +185,6 @@ if ($refresh_data){
 	download_data($outputfile, $URL);
 }
 
-$domains_blocked_counter=$domains_blocked_counter + process_data($outputfile, "web_block2", $debug, $theFiles, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
-
 //*******************************************************************************************************************
 //*******************************************************************************************************************
 //*******************************************************************************************************************
@@ -385,8 +210,6 @@ if ($refresh_data){
 	download_data($outputfile, $URL);
 }
 
-$domains_blocked_counter=$domains_blocked_counter + process_data($outputfile, "web_block3", $debug, $theFiles, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
-
 //*******************************************************************************************************************
 //*******************************************************************************************************************
 //*******************************************************************************************************************
@@ -404,7 +227,6 @@ if ($refresh_data){
 	$URL="https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt";
 	download_data($outputfile, $URL);
 }
-$domains_blocked_counter=$domains_blocked_counter + process_data($outputfile, "web_block4", $debug, $theFiles, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
 
 //*******************************************************************************************************************
 //*******************************************************************************************************************
@@ -448,9 +270,6 @@ if ($refresh_data){
 	download_data($outputfile, $URL);
 }
 
-
-$domains_blocked_counter=$domains_blocked_counter + process_data($outputfile, "web_block5", $debug, $theFiles, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
-
 //*******************************************************************************************************************
 //*******************************************************************************************************************
 //*******************************************************************************************************************
@@ -464,18 +283,7 @@ if ($refresh_data){
 	$URL="https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt";
 	download_data($outputfile, $URL);
 }
-$domains_blocked_counter=$domains_blocked_counter + process_data($outputfile, "web_block6", $debug, $theFiles, $email_address, $smtp_server, $SMTPAuth_type, $smtp_user, $smtp_pass, $SMTPSecure_type, $smtp_port, $from_email_address);
 
-//*******************************************************************************************************************
-//*******************************************************************************************************************
-//*******************************************************************************************************************
-//*******************************************************************************************************************
-
-print "<H1 style=\"color:red;\">TOTAL DOMAINS BLOCKED: ".$domains_blocked_counter."</H1>";
-
-//saving details of the total domains blocked count to inflixDB
-$post_url="".$measurement.",snmp_device_name=$snmp_device_name domains_blocked_counter=".$domains_blocked_counter."";
-
-shell_exec("curl -XPOST \"".$influxdb_http_type."://".$influxdb_host.":".$influxdb_port."/api/v2/write?bucket=".$influxdb_name."&org=".$influxdb_org."\" -H \"Authorization: Token ".$influxdb_pass."\" --data-raw \"".$post_url."\"");
+print shell_exec("bash webblock.sh");
 
 ?>
