4ee1f07fc02a4729316bf22b2c76e30cffff68c0
wallacebrf <62187108+wallacebrf@users.noreply.github.com>
2025-04-12 08:12:21 -0500

    Add files via upload

--- webblock.sh
+++ webblock.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
+# shellcheck disable=SC2219,SC2035
+#Version 4/11/2025
+#By Brian Wallace
+
+#ingest file parameters
+Working_Dir="/volume1/web/DNS_FG-91G"
 
 #influxDB parameters
 snmp_device_name="FG-91G"
 measurement="fortigate_blocked_domains"
 influxdb_host="localhost"
 influxdb_port="8086"
-influxdb_pass="your_password"
+influxdb_pass="xxxxxx"
 influxdb_name="db_name"
 influxdb_http_type="http"
-influxdb_org="home"
-
-#Email parameters
-email_address="email@email.com"
-from_email_address="email@email.com"
-notification_file_location="/volume1/web/logging/notifications"
-enable_notifications=1
+influxdb_org="my_org"
 
 #*************************************************************
 #*************************************************************
@@ -22,6 +22,35 @@ enable_notifications=1
 #*************************************************************
 #*************************************************************
 
+##########################################################################
+#create a lock file and temp directory directory to prevent more than one instance of this script from executing  at once
+##########################################################################
+if ! mkdir "$Working_Dir/tmp"; then
+	echo -e "Failed to acquire lock\n" >&2
+	exit 1
+fi
+trap 'rm -rf $Working_Dir/tmp/' EXIT #remove the lockdir on exit
+
+
+##########################################################################
+#create logging directory if does not exist and creating log file
+##########################################################################
+if [[ -d "$Working_Dir/log/" ]]; then
+	echo "Log directory \"$Working_Dir/log/\" exists"
+else
+	if ! mkdir "$Working_Dir/log"; then
+		echo -e "Failed to create log directory \"$Working_Dir/log/\"\n" >&2
+		exit 1
+	else
+		echo "Created Log directory \"$Working_Dir/log/\""
+	fi
+fi
+date=$(date '+%Y-%m-%d')
+echo "Log Date: $date" > "$Working_Dir/log/$date.txt"
+if [[ ! -w "$Working_Dir/log/$date.txt" ]]; then
+	echo -e "Failed to create log file \"$Working_Dir/log/$date.txt\"\n" >&2
+	exit 1
+fi
 
 #########################################################
 #this function pings google.com to confirm internet access is working prior to sending email notifications 
@@ -35,202 +64,122 @@ ping -c1 "google.com" > /dev/null #ping google.com
 		true
 	fi
 }
-#########################################################
-#this function is used to send notifications
-#########################################################
-function send_mail(){
-#email_last_sent_log_file=${1}			this file contains the UNIX time stamp of when the email is sent so we can track how long ago an email was last sent
-#message_text=${2}						this string of text contains the body of the email message
-#email_subject=${3}						this string of text contains the email subject line
-#email_contents_file=${4}				this file is where the contents of the email are saved prior to sending and it contains the log of the email transmission, either will indicated email sent successfully or will include the error details
-#error_message=${5}						this string of text is only displayed when the script is executed from the CLI, it will be part of the error message if the email is not sent correctly
-#email_interval=${6}					this numerical value will control how many minutes must pass before the next email is allowed to be sent
-#use_mail_plus_server=${7}				this will control if mail plus server (IE sendmail) or ssmtp will be used to send emails. ssmtp is much slower to execute but does not require the installation of mail plus server
-	if [[ $enable_notifications == 1 ]]; then
-		local message_tracker=""
-		local time_diff=0
-		echo -e "${2}"
-		echo ""
-		if check_internet; then
-			local current_time=$( date +%s )
-			if [ -r "${1}" ]; then #file is available and readable 
-				read message_tracker < "${1}"
-				time_diff=$((( $current_time - $message_tracker ) / 60 ))
-			else
-				echo -n "$current_time" > "${1}"
-				time_diff=$(( ${6} + 1 ))
-			fi
-					
-			if [ $time_diff -ge ${6} ]; then
-				local now=$(date +"%T")
-				echo "the email has not been sent in over ${6} minutes, re-sending email"
-				if [[ ${7} == 1 ]]; then #if this is a value of 1, use mail plus
-					#verify MailPlus Server package is installed and running as the "sendmail" command is not installed in Synology by default. the MailPlus Server package is required
-					local install_check=$(/usr/syno/bin/synopkg list | grep MailPlus-Server)
-					if [ "$install_check" != "" ];then
-						#"MailPlus Server is installed, verify it is running and not stopped"
-						local status=$(/usr/syno/bin/synopkg is_onoff "MailPlus-Server")
-						if [ "$status" = "package MailPlus-Server is turned on" ]; then
-							echo "from: $from_email_address " > "${4}"
-							echo "to: $email_address " >> "${4}"
-							echo "subject: ${3}" >> "${4}"
-							echo "" >> "${4}"
-							echo -e "$now - ${2}" >> "${4}" #adding the mail-body text. 
-							local email_response=$(sendmail -t < "${4}"  2>&1)
-							if [[ "$email_response" == "" ]]; then
-								echo "" |& tee -a "${4}"
-								echo -e "Email to \"$email_address\" Sent Successfully\n" |& tee -a "${4}"
-								message_tracker=$current_time
-								time_diff=0
-								echo -n "$message_tracker" > "${1}"
-							else
-								echo -e "Warning, an error occurred while sending the ${5} notification email. the error was: $email_response\n" |& tee -a "${4}"
-							fi
-						else
-							echo -e "Warning Mail Plus Server is Installed but not running, unable to send email notification\n" |& tee -a "${4}"
-						fi
-					else
-						echo -e "Mail Plus Server is not installed, unable to send email notification\n" |& tee -a "${4}"
-					fi
-				else #since the value is not equal to 1, use ssmtp command
-					echo "From: $from_email_address " > "${4}"
-					echo "Subject: ${3}" >> "${4}"
-					echo "" >> "${4}"
-					echo -e "\n$now - ${2}\n" >> "${4}" #adding the mail-body text. 
-					
-					#the "ssmtp" command can only take one email address destination at a time. so if there are more than one email addresses in the list, we need to send them one at a time
-					address_explode=(`echo $email_address | sed 's/;/\n/g'`) #explode on the semicolon separating the different possible addresses
-					local xx=0
-					for xx in "${!address_explode[@]}"; do
-						local email_response=$(ssmtp ${address_explode[$xx]} < "${4}"  2>&1)
-						if [[ "$email_response" == "" ]]; then
-							echo "" |& tee -a "${4}"
-							echo -e "Email to \"${address_explode[$xx]}\" Sent Successfully\n" |& tee -a "${4}"
-							message_tracker=$current_time
-							time_diff=0
-							echo -n "$message_tracker" > "${1}"
-						else
-							echo -e "Warning, an error occurred while sending the ${5} notification email. the error was: $email_response\n" |& tee -a "${4}"
-						fi
-					done
-				fi
-			else
-				echo -e "Only $time_diff minuets have passed since the last notification, email will be sent every ${6} minutes. $(( ${6} - $time_diff )) Minutes Remaining Until Next Email\n"
-			fi
-		else
-			echo -e "Internet is not available, skipping sending email\n" |& tee -a "${4}"
-		fi
-	else
-		echo "Unable to send notifications, pleas enable notifications in the web-interface"
-	fi
-}
 
 #########################################################
 #script main
 #########################################################
 
-current_files=$(ls "/volume1/web/DNS_FG-91G" | grep 'web_block.*\.txt')
+##########################################################################
+#download all of the Pie Hole Block List text files 
+##########################################################################
+if [[ ! -r "$Working_Dir/web_block_source.txt" ]]; then
+	echo -e "Unable to read required file \"$Working_Dir/web_block_source.txt\"\n" >&2
+	exit 1
+fi
 
-echo -e "Combining all downloaded files \"master1.txt master2.txt master3.txt master4.txt master5.txt master6.txt\" into file \"out.txt\"\n\n"
+echo -e "\n\n***************************************"  |& tee -a "$Working_Dir/log/$date.txt" 
+echo "download all of the Pie Hole Block List text files"  |& tee -a "$Working_Dir/log/$date.txt"
+echo -e "***************************************\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
-cat master1.txt master2.txt master3.txt master4.txt master5.txt master6.txt > out.txt
-num_lines=$(wc -l out.txt)
-num_lines=${num_lines::-8}
-echo -e "Files combined. Current blocked URLs is $num_lines\n\n"
+counter=1
+	
+while read -r line; do
+	let counter=counter+1
+	wget -q -O "$counter.txt" "${line//[$'\t\r\n ']}"
+	mv "$Working_Dir/$counter.txt" "$Working_Dir/tmp/$counter.txt"
+	num_lines=$(wc -l < "$Working_Dir/tmp/$counter.txt")
+	if [ "$num_lines" -eq 0 ]; then
+		#echo "$num_lines Subnets Downloaded from ${line//[$'\t\r\n ']}"  |& tee -a "$Working_Dir/log/$date.txt"
+	#else
+		echo -e "\nWARNING - ${line//[$'\t\r\n ']} Returned Zero Results\n"  |& tee -a "$Working_Dir/log/$date.txt"
+	fi
+	
+done < "$Working_Dir/web_block_source.txt"
+
+
+##########################################################################
+# Combine all text files
+##########################################################################
+echo -e "\n\n***************************************"  |& tee -a "$Working_Dir/log/$date.txt"
+echo "Combining all text files"  |& tee -a "$Working_Dir/log/$date.txt"
+echo -e "***************************************\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
+
+cd "$Working_Dir/tmp/" || exit 1
+cat *.txt > "$Working_Dir/master.txt"
+num_lines=$(wc -l < "$Working_Dir/master.txt")
+
+echo -e "Total Lines Of Data Downloaded: $num_lines\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
 if [[ $num_lines -eq 0 ]]; then
 	echo -e "number of lines is zero, something is wrong"
 	exit 1
 fi
 
+counter=1
+echo -e "Processing data to be compatable with Fortigate External Threat Feeds\n" |& tee -a "$Working_Dir/log/$date.txt"
+echo -e "Removing the following items: \"127.0.0.1 \", \"localhost\", \"::1 \", \"0.0.0.0 \", \"0.0.0.0\", \"127.0.0.1	\", \".localdomain\", \"255.255.255.255	broadcasthost\", \"::1\" , \"|\" , \"^\"\n\n" |& tee -a "$Working_Dir/log/$date.txt"
+sed -i -e 's/\(127.0.0.1 \|localhost\|::1 \|0.0.0.0 \|0.0.0.0\|.localdomain\|255.255.255.255	broadcasthost\|::1\)//g' "$Working_Dir/master.txt"
+num_lines=$(wc -l < "$Working_Dir/master.txt")
+echo -e "Data Removed - Current blocked URLs is $num_lines\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
-echo -e "Clearing all comment lines starting with \"#\""
-sed -i 's/#.*$//' out.txt #delete lines starting with # as those are comments
-num_lines=$(wc -l out.txt)
-num_lines=${num_lines::-8}
-echo -e "Comments removed. Current blocked URLs is $num_lines\n\n"
+echo -e "Clearing all comment lines starting with \"#\""  |& tee -a "$Working_Dir/log/$date.txt"
+sed -i 's/#.*$//' "$Working_Dir/master.txt" #delete lines starting with # as those are comments
+num_lines=$(wc -l < "$Working_Dir/master.txt")
+echo -e "Data Removed - Current blocked URLs is $num_lines\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
 if [[ $num_lines -eq 0 ]]; then
-	echo -e "number of lines is zero, something is wrong"
+	echo -e "number of lines is zero, something is wrong"  |& tee -a "$Working_Dir/log/$date.txt"
 	exit 1
 fi
 
-echo -e "Clearing all lines starting with \"!\""
-sed -i 's/!.*$//' out.txt #delete lines starting with ! as those are comments
-num_lines=$(wc -l out.txt)
-num_lines=${num_lines::-8}
-echo -e "lines starting with \"!\" removed. Current blocked URLs is $num_lines\n\n"
+echo -e "Deleting all Empty/Cleared Lines"  |& tee -a "$Working_Dir/log/$date.txt"
+sed -i '/^\s*$/d' "$Working_Dir/master.txt" #delete empty lines
+num_lines=$(wc -l < "$Working_Dir/master.txt")
+echo -e "Data Removed - Current blocked URLs is $num_lines\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
 if [[ $num_lines -eq 0 ]]; then
-	echo -e "number of lines is zero, something is wrong"
+	echo -e "number of lines is zero, something is wrong"  |& tee -a "$Working_Dir/log/$date.txt"
 	exit 1
 fi
 
-echo -e "Clearing all lines starting with \"|\""
-sed -i 's/|.*$//' out.txt #delete lines starting with | as those are comments
-num_lines=$(wc -l out.txt)
-num_lines=${num_lines::-8}
-echo -e "lines starting with \"|\" removed. Current blocked URLs is $num_lines\n\n"
+echo -e "Deleting all other instances of \"!\" \"|\" \"^\" \"?\" \"=\" and \" \" within the file as these are not allowable URL characters"  |& tee -a "$Working_Dir/log/$date.txt"
+sed -i 's|[|!^?= },]||g' "$Working_Dir/master.txt"
+num_lines=$(wc -l < "$Working_Dir/master.txt")
+echo -e "Data Removed - Current blocked URLs is $num_lines\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
 if [[ $num_lines -eq 0 ]]; then
-	echo -e "number of lines is zero, something is wrong"
+	echo -e "number of lines is zero, something is wrong"  |& tee -a "$Working_Dir/log/$date.txt"
 	exit 1
 fi
 
-echo -e "Deleting all Empty/Cleared Lines"
-sed -i '/^\s*$/d' out.txt #delete empty lines
-num_lines=$(wc -l out.txt)
-num_lines=${num_lines::-8}
-echo -e "Empty lines removed. Current blocked URLs is $num_lines\n\n"
+echo -e "Deleting all duplicate entries"  |& tee -a "$Working_Dir/log/$date.txt"
+awk -i inplace '!seen[$0]++' "$Working_Dir/master.txt" # delete duplicates 
+num_lines=$(wc -l < "$Working_Dir/master.txt")
+echo -e "Duplicate lines removed. Final Total blocked URLs is $num_lines\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
 if [[ $num_lines -eq 0 ]]; then
-	echo -e "number of lines is zero, something is wrong"
-	exit 1
-fi
-
-echo -e "Deleting all other instances of \"!\" \"|\" \"^\" \"?\" \"=\" and \" \" within the file as these are not allowable URL characters"
-sed -i 's|[|!^?= ,]||g' out.txt
-num_lines=$(wc -l out.txt)
-num_lines=${num_lines::-8}
-echo -e "Characters removed. Current blocked URLs is $num_lines\n\n"
-
-if [[ $num_lines -eq 0 ]]; then
-	echo -e "number of lines is zero, something is wrong"
-	exit 1
-fi
-
-echo -e "Deleting all duplicate entries"
-awk -i inplace '!seen[$0]++' out.txt # delete duplicates 
-num_lines=$(wc -l out.txt)
-num_lines=${num_lines::-8}
-echo -e "Duplicate lines removed. Final Total blocked URLs is $num_lines\n\n"
-
-if [[ $num_lines -eq 0 ]]; then
-	echo -e "number of lines is zero, something is wrong"
+	echo -e "number of lines is zero, something is wrong"  |& tee -a "$Working_Dir/log/$date.txt"
 	exit 1
 fi
 
 
-echo -e "splitting results into separate files containing no more than 131,000 entries\n\n"
+echo -e "splitting results into separate files containing no more than 131,000 entries\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 lines=0
 file_name_counter=0
-echo -e "saving entries to web_block0.txt\n"
-echo "" > "web_block0.txt" #reset the contents if old contents were there before
-while read line; do
-	echo "$line" >> "web_block$file_name_counter.txt"
+echo -e "saving entries to $Working_Dir/web_block0.txt\n"  |& tee -a "$Working_Dir/log/$date.txt"
+echo "" > "$Working_Dir/web_block0.txt" #reset the contents if old contents were there before
+while read -r line; do
+	echo "$line" >> "$Working_Dir/web_block$file_name_counter.txt"
 	let lines=lines+1
 	if [[ $lines -gt 131000 ]]; then
 		let file_name_counter=file_name_counter+1
-		echo -e "saving entries to web_block$file_name_counter.txt\n"
-		file_check=$(echo "$current_files" | grep web_block$file_name_counter.txt)
-		if [ "$file_check" == "" ];then
-			echo -e "\nfile web_block$file_name_counter.txt is new!\n"
-			send_mail "$notification_file_location/DNS_block_list_email_last_sent_tracker.txt" "File web_block$file_name_counter.txt is new!. Update the firewall settings to include this new file" "New web_block File Name Available" "$notification_file_location/DNS_blocklist_email_notification_file.txt" "" 0 1
-		fi
+		echo -e "saving entries to $Working_Dir/web_block$file_name_counter.txt\n"  |& tee -a "$Working_Dir/log/$date.txt"
 		lines=0
-		echo "" > "web_block$file_name_counter.txt" #reset the contents if old contents were there before
+		echo "" > "$Working_Dir/web_block$file_name_counter.txt" #reset the contents if old contents were there before
 	fi
-done < out.txt
+done < "$Working_Dir/master.txt"
+
+echo -e "Web Block Processing Complete. Final Total blocked URLs is $num_lines\n\n"  |& tee -a "$Working_Dir/log/$date.txt"
 
 #Post stats to influxdb
 post_url="$measurement,snmp_device_name=$snmp_device_name num_lines=$num_lines"
